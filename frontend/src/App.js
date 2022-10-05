8dede7fe0b0a79fae2c6b6e2f0674eeb51099846
michaelgetachew-abebe <mikygetyos@gmail.com>
2022-10-05 21:45:53 +0000

    Feat(add): aligned the Header to the center

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello World!!!</h1>
+    <div>
+      <div className="App">
+      <h1>Hello World!!!</h1>
+      </div>
+    </div>
   );
 }
 
