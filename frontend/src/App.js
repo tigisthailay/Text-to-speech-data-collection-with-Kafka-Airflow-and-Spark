228fa7086bf188d45e1716b5efa6cd73d3f346f2
michaelgetachew-abebe <mikygetyos@gmail.com>
2022-10-05 21:59:05 +0000

    Feat(add): adde the hello world header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Header from './components/Header/header';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <div className="App">
       <h1>Hello World!!!</h1>
       </div>
+      <Header />
     </div>
   );
 }
--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,3 +1,7 @@
 import React from 'react'
-
 // Import an image for logo here
+
+const Header = () =>(
+    <h1>Hello World!!!</h1>
+);
+export default Header;
